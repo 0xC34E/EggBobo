26ae17481994476f87306a9b3475a100450a9b9e
0xC34E <69095444+0xC34E@users.noreply.github.com>
2021-01-01 17:06:24 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -149,7 +149,7 @@ class Commands {
 
     static PycckaRPyletka(message) {
         let chance = getRandomInt(0, 5);
-        let emoji = message.guild.emojis.find(emoji => emoji.name === "eggboborr");
+        let emoji = message.guild.emojis.cache.find(emoji => emoji.name === "eggboborr");
         let mess = message.channel.send(`${emoji}`).then(mess=>{
             let user = message.mentions.members.first();
             setTimeout(function(){
